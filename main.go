c4b34a34d0ccda8c40c43bdfcccb46bd6cd86437
muizu <muizu0920555@protonmail.com>
2023-10-01 01:44:04 +0900

    get-gin

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"data": "this is the register endpoint.",
+		})
+	})
+	router.Run(":8080")
+}
